2fc481030e7ead419213084b07b297f64b0a7e4b
Joseth Calderon <stevencorredor04@gmail.com>
2024-02-17 19:06:41 -0500

    Frontend UI

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './assets/components/header'
+import MainLanding from './assets/components/mainLanding'
+import Footer from './assets/components/footer'
+
+function App (){
+  return (
+    <>
+      <Header />
+      <MainLanding />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/assets/components/Components__Main/advantagesMainLanding.jsx
@@ -0,0 +1,43 @@
+// Images section Advantages
+import AdvantagesSVG1 from '../../svg/AdvantagesInfo1.svg'
+import AdvantagesSVG2 from '../../svg/AdvantagesInfo2.svg'
+import AdvantagesSVG3 from '../../svg/AdvantagesInfo3.svg'
+import advantagesSVG from '../../svg/mainAdvantages.svg'
+
+export default function AdvantagesMainLanding(){
+  return(
+    <>
+      <section className="main__advantages">
+        <div className="main__advantages--svg">
+          <img src={advantagesSVG} alt="Union hace la fuerza" />
+        </div>
+        <div className="main__advantages--info">
+          <h2>Creada para todos</h2>
+          <p>Te enseñamos todo lo que tienes que saber sobre la salud mental y el bienestar emocional por medio de módulos educativos que tu necesites sobre:</p>
+          <ul className="main__advantages--info-list">
+            <li>Como sanar cualquier emoción</li>
+            <li>Salud mental básica</li>
+            <li>Superar la depresión</li>
+            <li>Enfrentar la ansiedad</li>
+            <li>Mejorar tu autoestima</li>
+            <li>Mejores relaciones interpersonales</li>
+          </ul>
+          <div className="main__advantages--info-svg">
+            <div className="info__svg1">
+              <img src={AdvantagesSVG1} alt="Seguridad" />
+              <p>Herramienta única</p>
+            </div>
+            <div className="info__svg2">
+              <img src={AdvantagesSVG2} alt="Flexibidad" />
+              <p>Archivo integrados</p>
+            </div>
+            <div className="info__svg3">
+              <img src={AdvantagesSVG3} alt="Mejor Metodologia" />
+              <p>Simple y sencillo</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
