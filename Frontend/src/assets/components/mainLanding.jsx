35930ddeb962f11d155d51404368502453c62096
Joseth Calderon <stevencorredor04@gmail.com>
2024-02-17 20:57:53 -0500

    Implemnetando nuevos estilos y textos

--- Frontend/src/assets/components/mainLanding.jsx
+++ Frontend/src/assets/components/mainLanding.jsx
@@ -4,7 +4,6 @@ import CoursesWebsite from "./Components__Main/courseMainLanding"
 import AdvantagesMainLanding from "./Components__Main/advantagesMainLanding"
 import ServicesMainLanding from "./Components__Main/servicesMainLanding"
 import PricesMainLanding from "./Components__Main/pricesMainLanding"
-import OpinionMainLanding from "./Components__Main/opinionsMainLanding"
 import EndingMainLanding from "./Components__Main/endingMainLanding"
 
 /* Images and SVGs */
@@ -22,8 +21,8 @@ export default function MainLanding (){
         <section className="main__description--background">
           <section className="main__description">
             <div className="main__description--info">
-              <h2>¡Únete a nosotros en este camino hacia una vida más feliz y saludable!</h2>
-              <p>Nuestra plataforma de salud mental enseña herramientas terapéuticas para mejorar tu vida emocional. A través de nuestro programa, aprenderás habilidades para controlar el estrés y la ansiedad, superar patrones de pensamiento negativo y depresivos, mejorar tus relaciones interpersonales y más.</p>
+              <h2>Estudiar es mucho mas sencillo cuando obtienes algo a cambio</h2>
+              <p>Cada minuto que dedicas al estudio es una inversión en ti mismo. Nuestro innovador sistema recompensa tu esfuerzo académico. A medida que te sumerges en cursos y adquieres nuevos conocimientos, tu inversión inicial se multiplica. ¡Aprender se convierte en una experiencia lucrativa!</p>
             </div>
             <div className="main__description--video">
               <img src={Image1} alt="Mujeres Felices / Foto de Andrea Piacquadio" />
@@ -34,7 +33,6 @@ export default function MainLanding (){
         <AdvantagesMainLanding />
         <ServicesMainLanding />
         <PricesMainLanding />
-        <OpinionMainLanding />
         <EndingMainLanding />
       </main>
     </>
